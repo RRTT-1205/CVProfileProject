9958031611365004fd86b54840a0ed3ea721bad1
Rafael Rodriguez <102380594+RRTT-1205@users.noreply.github.com>
2022-08-27 01:04:36 -0300

    header2

--- CVProfileProject/src/app/app.module.ts
+++ CVProfileProject/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ImgFondoHeaderComponent } from './components/img-fondo-header/img-fondo-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ImgFondoHeaderComponent
   ],
   imports: [
     BrowserModule,
