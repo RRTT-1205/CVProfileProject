2a9fd9f767acb43b8fe780f72ccbf8421d908b4f
Rafael Rodriguez <102380594+RRTT-1205@users.noreply.github.com>
2022-09-01 20:36:02 -0300

    backEnd04

--- BackEnd/src/main/java/com/profilert/rrt/Entidad/Persona.java
+++ BackEnd/src/main/java/com/profilert/rrt/Entidad/Persona.java
@@ -1,5 +1,6 @@
 package com.profilert.rrt.Entidad;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,7 +13,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-public class Persona {
+public class Persona implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
