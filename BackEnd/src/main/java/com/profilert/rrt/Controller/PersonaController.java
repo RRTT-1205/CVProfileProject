bc752e412346d46e2f3d89d6fb37d7139f94a423
Rafael Rodriguez <102380594+RRTT-1205@users.noreply.github.com>
2022-09-01 22:30:10 -0300

    backEnd05

--- BackEnd/src/main/java/com/profilert/rrt/Controller/PersonaController.java
+++ BackEnd/src/main/java/com/profilert/rrt/Controller/PersonaController.java
@@ -4,8 +4,7 @@ import com.profilert.rrt.Entidad.Persona;
 import com.profilert.rrt.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,51 +15,48 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+
 public class PersonaController {
 
     @Autowired
-    IPersonaService ipersonaservice;
+    IPersonaService ipersonaService;
 
     @GetMapping("personas/traer")
     public List<Persona> getPersona() {
-        return ipersonaservice.getPersona();
+        return ipersonaService.getPersona();
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona) {
-        ipersonaservice.savePersona(persona);
+        ipersonaService.savePersona(persona);
         return "La persona fue creada correctamente";
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id) {
-        ipersonaservice.deletePersona(id);
+        ipersonaService.deletePersona(id);
         return "La persona fue eliminada";
     }
 
     //URL:Puerto/personas/editar/4/nombre & apellido & img
-    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
             @RequestParam("nombre") String nuevoNombre,
             @RequestParam("apellido") String nuevoApellido,
             @RequestParam("img") String nuevoImg) {
-        Persona persona = ipersonaservice.findPersona(id);
+        Persona persona = ipersonaService.findPersona(id);
 
         persona.setNombre(nuevoNombre);
         persona.setApellido(nuevoApellido);
         persona.setImg(nuevoImg);
 
-        ipersonaservice.savePersona(persona);
+        ipersonaService.savePersona(persona);
         return persona;
     }
 
     @GetMapping("personas/traer/perfil")
     public Persona findPersona() {
-        return ipersonaservice.findPersona((long) 1);
+        return ipersonaService.findPersona((long) 1);
     }
 
 }
--- BackEnd/src/main/java/com/profilert/rrt/Interface/IPersonaService.java
+++ BackEnd/src/main/java/com/profilert/rrt/Interface/IPersonaService.java
@@ -5,16 +5,12 @@ import java.util.List;
 
 public interface IPersonaService {
 
-    //Traer Lista de Personas(Desde Persona)
     public List<Persona> getPersona();
 
-    //Guardar Persona
     public void savePersona(Persona persona);
 
-    //Eliminar
     public void deletePersona(Long id);
 
-    //Buscar
     public Persona findPersona(Long id);
 
 }
