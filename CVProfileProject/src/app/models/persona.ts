135b7a98a8d128229e6fef7e60d24e3cd3c1b8de
Rafael Rodriguez <102380594+RRTT-1205@users.noreply.github.com>
2022-09-03 22:30:06 -0300

    frontEnd08

--- CVProfileProject/src/app/models/persona.ts
+++ CVProfileProject/src/app/models/persona.ts
@@ -1,2 +1,12 @@
-export class Persona {
+export class persona{
+    id?: number;
+    nombre: String;
+    apellido: String;
+    img: String;
+
+    constructor(nombre: String,apellido: String, img: String){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.img = img;
+    }
 }
--- CVProfileProject/src/app/services/persona.service.ts
+++ CVProfileProject/src/app/services/persona.service.ts
@@ -1,19 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-
-
+import { Observable } from 'rxjs';
+import { persona } from '../models/persona';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PersonaService {
-  URL = 'https://';
 
+export class PersonaService {
+  URL = '';
 
   constructor(private http: HttpClient) { }
 
- /* public getPersona(): Observable<Persona> {
+  public getPersona(): Observable<persona>{
     return this.http.get<persona>(this.URL+ 'traer/perfil');
-  }*/
+  }
 }
