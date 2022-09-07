1f0bdb4e45f070ca0f361c661248a27fecf3026a
Rafael Rodriguez <102380594+RRTT-1205@users.noreply.github.com>
2022-09-06 23:52:02 -0300

    frontEnd16

--- CVProfileProject/src/app/app-routing.module.ts
+++ CVProfileProject/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia/new-experiencia.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: '', redirectTo:'login', pathMatch:'full'}
+  {path: '', redirectTo:'login', pathMatch:'full'},
+  { path: 'nuevaexp', component: NewExperienciaComponent}
 ];
 
 @NgModule({
